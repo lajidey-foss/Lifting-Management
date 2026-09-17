672badb7852ef78d4136b3b31a4d882c9bb21bc3
lajidey-foss <spryng.managed@gmail.com>
2026-09-17 15:50:39 +0000

    Allocation and Wallet script
    Create Allocation and Allocation Details Doctype, Provided needed server and client script for payment wallet to create new truckon from pw

--- lifting_management/lifting/doctype/payment_wallet/payment_wallet.js
+++ lifting_management/lifting/doctype/payment_wallet/payment_wallet.js
@@ -1,8 +1,21 @@
 // Copyright (c) 2026, Jide Olayinka [Pivotage Integrated] and contributors
 // For license information, please see license.txt
 
-// frappe.ui.form.on("Payment Wallet", {
-// 	refresh(frm) {
+frappe.ui.form.on("Payment Wallet", {
+	refresh(frm) {
+        if(!frm.is_new()) {
+			frm.add_custom_button(__('Truckon'),
+				function() {
+					frm.trigger("make_truckons")
+				}, __('Create'));	
+			
+		}
 
-// 	},
-// });
+	},
+    make_truckons: function(frm) {
+		frappe.model.open_mapped_doc({
+			method: "lifting_management.lifting.doctype.payment_wallet.payment_wallet.create_truckon",
+			frm: frm
+		})
+	},
+});
